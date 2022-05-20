9e1b69238cf0401909af9b07b5d8b5579a9898f0
shishir1604 <98305285+shishir1604@users.noreply.github.com>
2022-05-20 18:32:49 +0530

    Update loancalc.java

--- loancalc.java
+++ loancalc.java
@@ -1,20 +1,20 @@
 public class loancalc 
-{
+{//to calculate loan 
     static double amt,rate,ci,si;
     static int time;
-    loancalc(double A,double R,int T)
+    loancalc(double A,double R,int T)// constructor to initialise
     {
         this.amt=A;
         this.rate=R;
         this.time=T;
     }    
-    static double compound()
+    static double compound()// to calculate compound interest
     {
         double x=(1+0.01*rate);
         ci=Math.pow(x, time)*amt;
         return (ci+amt);
     }
-    static double simple()
+    static double simple()//to calculate simple interest
     {
         si=amt*rate*time/100;
         return (si+amt);
