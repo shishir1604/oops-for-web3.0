75a1203dc2775ab163cdb1944cdf5e2df1e9b133
shishir1604 <98305285+shishir1604@users.noreply.github.com>
2022-05-20 18:37:25 +0530

    Update main.java

--- main.java
+++ main.java
@@ -1,11 +1,14 @@
 import java.util.Scanner;
+// this is the main class to be executed 
+// it lets us create number of instance of our loan service project 
+// thus we can serve various customers.
 public class main 
 {
     public static void main(String[] args) 
     {
         System.out.println("Enter the number of customers");    
         int n=new Scanner(System.in).nextInt();
-        execute c[]=new execute[n];
+        execute c[]=new execute[n];// array of objects.
         for(int i=0;i<n;i++)
         {
             c[i]=new execute();
