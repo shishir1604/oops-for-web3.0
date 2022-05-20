5666bf680b0b35b80ee1b90d2352f32dd46667fb
shishir1604 <98305285+shishir1604@users.noreply.github.com>
2022-05-20 18:30:47 +0530

    Update execute.java

--- execute.java
+++ execute.java
@@ -1,13 +1,13 @@
 public class execute
-{
+{//to call objects of required classes and carry out the loaning.
     public static void output()
     {
         double amot=0.0,i=0.0;
-        schemes a=new schemes();
-        a.acc_detail();
+        schemes a=new schemes();//object of scheme class
+        a.acc_detail();//calls function of super class i.e.account class
         a.deatils();
 
-        loancalc object=new loancalc(a.amt, a.rate,a.time);
+        loancalc object=new loancalc(a.amt, a.rate,a.time);//object of loancalc class it initialises variables with certain inputs.
         
         if(a.c==0)
         {amot=object.compound();i=object.ci;}
